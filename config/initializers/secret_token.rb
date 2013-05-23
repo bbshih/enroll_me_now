8ae9bb06cbdf1342d126314672ffc54cfeb2ff10
Billy Shih <me@billyshih.com>
2013-05-22 22:11:56 -0700

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnrollMeNow::Application.config.secret_token = 'ad61cbc3b4cd4b6f135706346cfff9bb56aa289254d0ad8a40ac641ae697c325afdaf2521c404203b4354414c6709cd114a65a5ca54ea553e5088973cc92d5c3'
